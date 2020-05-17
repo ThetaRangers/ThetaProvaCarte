812564d515b0f71d0335ecf245205f2b123a535b
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-05-17 14:21:19 +0200

    Add dark theme

--- app/src/main/java/it/ferrarally/provacarte/MainActivity.java
+++ app/src/main/java/it/ferrarally/provacarte/MainActivity.java
@@ -1,21 +1,28 @@
 package it.ferrarally.provacarte;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RadioGroup;
 
 import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Boolean isNightMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        isNightMode = (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK)
+                == Configuration.UI_MODE_NIGHT_YES;
         setContentView(R.layout.activity_main);
 
         new Holder();
@@ -68,6 +75,18 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, SwipeCard.class);
 
                     startActivity(intent);
+                }
+            });
+
+            final ImageButton buttonTheme = findViewById(R.id.btnTheme);
+            buttonTheme.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isNightMode) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    }
                 }
             });
         }
